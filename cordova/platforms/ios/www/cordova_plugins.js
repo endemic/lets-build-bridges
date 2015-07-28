b94fec19e1e1bfe7701c5a1596b63382c1eebe34
Nathan Demick <ndemick@gmail.com>
2015-07-28 07:19:09 -0400

    Re-building cordova project

---
+++ cordova/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,41 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cc.fovea.cordova.purchase/www/store-ios.js",
+        "id": "cc.fovea.cordova.purchase.InAppPurchase",
+        "clobbers": [
+            "store"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cc.fovea.cordova.purchase": "3.11.0",
+    "cordova-plugin-console": "1.0.1",
+    "cordova-plugin-splashscreen": "2.1.0"
+}
+// BOTTOM OF METADATA
+});
